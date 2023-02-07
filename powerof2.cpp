86d526791d4b46483880995e2b4604d8d271a808
vidhi <78697483+vidhi-kiet@users.noreply.github.com>
2023-02-08 01:14:30 +0530

    Add files via upload

---
+++ powerof2.cpp
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cout<<"enter a no.";
+    cin>>n;
+    if((n & (n-1))==0)
+    {
+        cout<<"yes";
+    }
+    else
+    {
+        cout<<"no";
+    }
+
+    return 0;
+}
